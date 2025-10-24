b5388fd3b290624f92bd1de15bb356cf7800b342
Iago-A <iblancoc23@fpcoruna.afundacion.org>
2025-10-24 17:56:12 +0200

    Lesson 10
    Implementation of an API REST

---
+++ src/main/java/com/iagobc/cursoSpringBoot/controllers/CustomerController.java
@@ -0,0 +1,27 @@
+package com.iagobc.cursoSpringBoot.controllers;
+
+import com.iagobc.cursoSpringBoot.domain.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    // Simulate data from database
+    private List<Customer> customers = new ArrayList<>(Arrays.asList(
+            new Customer(1, "Laura García", "LauraGF", "1234"),
+            new Customer(2, "Iago Blanco", "Iago-A", "4321"),
+            new Customer(3, "Marta Vilas", "mV1las", "6789"),
+            new Customer(4, "Jesus Largo", "Jeguel", "9876")
+    ));
+
+    @GetMapping ("/customers")
+    public List<Customer> getCustomers () {
+
+        return customers;
+    }
+}
